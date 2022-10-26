f011472c90b8058d22e2afc8df4f2baf8563c52a
menezesphill <phillipe.o.m@hotmail.com>
2022-10-26 16:48:57 -0300

    initial commit

---
+++ migrations/1_migration_authbank.js
@@ -0,0 +1,22 @@
+const NFTCreator = artifacts.require("GenNFT");
+const AuthBank = artifacts.require("AuthBank");
+
+
+module.exports = function (deployer) {
+    /* deployer.deploy(
+      NFTCreator,
+      20,
+      1,
+      50,
+      1648739423,
+      1648739423,
+      "https://gateway.pinata.cloud/ipfs/QmTNpSVs3MhWKYPUf47UsCK5yc96JwExZkVf3KyuRtQAKz",
+      "https://gateway.pinata.cloud/ipfs/QmSVyoTFpi9jepZke4pMtuCm5dWY71fJka5qPJ2qkqwgvW/",
+      ".json",
+      false,
+      true
+      ).then(
+        () => deployer.deploy(AuthBank)
+      ); */
+    deployer.deploy(AuthBank);
+  };
